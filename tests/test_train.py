0ec02691de5c6f8217cf816826f612998fb3db85
mokchhedul.islam@spectrum-bd.com <mokchheduls46@gmail.com>
2026-08-24 12:10:08 +0600

    add ci error fixed code

--- tests/test_train.py
+++ tests/test_train.py
@@ -6,7 +6,9 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-from src.train import get_models, train_model
+# from src.train import get_models, train_model
+
+from src.training.train import get_models, train_model
 
 
 def test_get_models():
